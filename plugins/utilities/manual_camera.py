c26342d320932380d79079087d1211b99ff443b3
Freaku17 <Freaku17@users.noreply.github.com>
2023-08-05 17:46:09 +0000

    [ci] auto-format

--- plugins/utilities/manual_camera.py
+++ plugins/utilities/manual_camera.py
@@ -8,7 +8,8 @@
 
 
 from __future__ import annotations
-import _babase, babase
+import _babase
+import babase
 import bascenev1 as bs
 import bauiv1 as bui
 from bauiv1lib.mainmenu import MainMenuWindow
@@ -16,144 +17,145 @@ from bauiv1lib.mainmenu import MainMenuWindow
 
 class Manual_camera_window:
     def __init__(self):
-            self._root_widget = bui.containerwidget(
-                               on_outside_click_call=None,
-                               size=(0,0))
-            button_size = (50,50)
-            self._text = bui.textwidget(parent=self._root_widget,
-                                         scale=0.65,
-                                         color = (0.75, 0.75, 0.75),
-                                         text='Cam Position',
-                                         size=(0, 0),
-                                         position=(500, 185),
-                                         h_align='center',
-                                         v_align='center')
-            self._xminus = bui.buttonwidget(parent=self._root_widget,
-                              size=button_size,
-                              label=babase.charstr(babase.SpecialChar.LEFT_ARROW),
-                              repeat = True,
-                              button_type='square',
-                              autoselect=True,
-                              position=(429, 60),
-                              on_activate_call=babase.Call(self._change_camera_position, 'x-'))
-            self._xplus = bui.buttonwidget(parent=self._root_widget,
-                              size=button_size,
-                              label=babase.charstr(babase.SpecialChar.RIGHT_ARROW),
-                              repeat = True,
-                              button_type='square',
-                              autoselect=True,
-                              position=(538, 60),
-                              on_activate_call=babase.Call(self._change_camera_position, 'x'))
-            self._yplus = bui.buttonwidget(parent=self._root_widget,
-                              size=button_size,
-                              label=babase.charstr(babase.SpecialChar.UP_ARROW),
-                              repeat = True,
-                              button_type='square',
-                              autoselect=True,
-                              position=(482, 120),
-                              on_activate_call=babase.Call(self._change_camera_position, 'y'))
-            self._yminus = bui.buttonwidget(parent=self._root_widget,
-                              size=button_size,
-                              label=babase.charstr(babase.SpecialChar.DOWN_ARROW),
-                              repeat = True,
-                              button_type='square',
-                              autoselect=True,
-                              position=(482, 2),
-                              on_activate_call=babase.Call(self._change_camera_position, 'y-'))
-            self.inwards = bui.buttonwidget(parent=self._root_widget,
-                              size=(100,30),
-                              label='Zoom +',
-                              repeat = True,
-                              button_type='square',
-                              autoselect=True,
-                              position=(-550, -60),
-                              on_activate_call=babase.Call(self._change_camera_position, 'z-'))
-            self._outwards = bui.buttonwidget(parent=self._root_widget,
-                              size=(100,30),
-                              label='Zoom -',
-                              repeat = True,
-                              button_type='square',
-                              autoselect=True,
-                              position=(-550, -100),
-                              on_activate_call=babase.Call(self._change_camera_position, 'z'))
-            self.target_text = bui.textwidget(parent=self._root_widget,
-                                         scale=0.65,
-                                         color = (0.75, 0.75, 0.75),
-                                         text='Cam Angle',
-                                         size=(0, 0),
-                                         position=(-462, 185),
-                                         h_align='center',
-                                         v_align='center')
-            self.target_xminus = bui.buttonwidget(parent=self._root_widget,
-                              size=button_size,
-                              label=babase.charstr(babase.SpecialChar.LEFT_ARROW),
-                              repeat = True,
-                              button_type='square',
-                              autoselect=True,
-                              position=(-538, 60),
-                              on_activate_call=babase.Call(self._change_camera_target, 'x-'))
-            self.target_xplus = bui.buttonwidget(parent=self._root_widget,
-                              size=button_size,
-                              label=babase.charstr(babase.SpecialChar.RIGHT_ARROW),
-                              repeat = True,
-                              button_type='square',
-                              autoselect=True,
-                              position=(-429, 60),
-                              on_activate_call=babase.Call(self._change_camera_target, 'x'))
-            self.target_yplus = bui.buttonwidget(parent=self._root_widget,
-                              size=button_size,
-                              label=babase.charstr(babase.SpecialChar.UP_ARROW),
-                              repeat = True,
-                              button_type='square',
-                              autoselect=True,
-                              position=(-482, 120),
-                              on_activate_call=babase.Call(self._change_camera_target, 'y'))
-            self.target_yminus = bui.buttonwidget(parent=self._root_widget,
-                              size=button_size,
-                              label=babase.charstr(babase.SpecialChar.DOWN_ARROW),
-                              repeat = True,
-                              button_type='square',
-                              autoselect=True,
-                              position=(-482, 2),
-                              on_activate_call=babase.Call(self._change_camera_target, 'y-'))                 
-            self._step_text = bui.textwidget(parent=self._root_widget,
+        self._root_widget = bui.containerwidget(
+            on_outside_click_call=None,
+            size=(0, 0))
+        button_size = (50, 50)
+        self._text = bui.textwidget(parent=self._root_widget,
+                                    scale=0.65,
+                                    color=(0.75, 0.75, 0.75),
+                                    text='Cam Position',
+                                    size=(0, 0),
+                                    position=(500, 185),
+                                    h_align='center',
+                                    v_align='center')
+        self._xminus = bui.buttonwidget(parent=self._root_widget,
+                                        size=button_size,
+                                        label=babase.charstr(babase.SpecialChar.LEFT_ARROW),
+                                        repeat=True,
+                                        button_type='square',
+                                        autoselect=True,
+                                        position=(429, 60),
+                                        on_activate_call=babase.Call(self._change_camera_position, 'x-'))
+        self._xplus = bui.buttonwidget(parent=self._root_widget,
+                                       size=button_size,
+                                       label=babase.charstr(babase.SpecialChar.RIGHT_ARROW),
+                                       repeat=True,
+                                       button_type='square',
+                                       autoselect=True,
+                                       position=(538, 60),
+                                       on_activate_call=babase.Call(self._change_camera_position, 'x'))
+        self._yplus = bui.buttonwidget(parent=self._root_widget,
+                                       size=button_size,
+                                       label=babase.charstr(babase.SpecialChar.UP_ARROW),
+                                       repeat=True,
+                                       button_type='square',
+                                       autoselect=True,
+                                       position=(482, 120),
+                                       on_activate_call=babase.Call(self._change_camera_position, 'y'))
+        self._yminus = bui.buttonwidget(parent=self._root_widget,
+                                        size=button_size,
+                                        label=babase.charstr(babase.SpecialChar.DOWN_ARROW),
+                                        repeat=True,
+                                        button_type='square',
+                                        autoselect=True,
+                                        position=(482, 2),
+                                        on_activate_call=babase.Call(self._change_camera_position, 'y-'))
+        self.inwards = bui.buttonwidget(parent=self._root_widget,
+                                        size=(100, 30),
+                                        label='Zoom +',
+                                        repeat=True,
+                                        button_type='square',
+                                        autoselect=True,
+                                        position=(-550, -60),
+                                        on_activate_call=babase.Call(self._change_camera_position, 'z-'))
+        self._outwards = bui.buttonwidget(parent=self._root_widget,
+                                          size=(100, 30),
+                                          label='Zoom -',
+                                          repeat=True,
+                                          button_type='square',
+                                          autoselect=True,
+                                          position=(-550, -100),
+                                          on_activate_call=babase.Call(self._change_camera_position, 'z'))
+        self.target_text = bui.textwidget(parent=self._root_widget,
+                                          scale=0.65,
+                                          color=(0.75, 0.75, 0.75),
+                                          text='Cam Angle',
+                                          size=(0, 0),
+                                          position=(-462, 185),
+                                          h_align='center',
+                                          v_align='center')
+        self.target_xminus = bui.buttonwidget(parent=self._root_widget,
+                                              size=button_size,
+                                              label=babase.charstr(babase.SpecialChar.LEFT_ARROW),
+                                              repeat=True,
+                                              button_type='square',
+                                              autoselect=True,
+                                              position=(-538, 60),
+                                              on_activate_call=babase.Call(self._change_camera_target, 'x-'))
+        self.target_xplus = bui.buttonwidget(parent=self._root_widget,
+                                             size=button_size,
+                                             label=babase.charstr(babase.SpecialChar.RIGHT_ARROW),
+                                             repeat=True,
+                                             button_type='square',
+                                             autoselect=True,
+                                             position=(-429, 60),
+                                             on_activate_call=babase.Call(self._change_camera_target, 'x'))
+        self.target_yplus = bui.buttonwidget(parent=self._root_widget,
+                                             size=button_size,
+                                             label=babase.charstr(babase.SpecialChar.UP_ARROW),
+                                             repeat=True,
+                                             button_type='square',
+                                             autoselect=True,
+                                             position=(-482, 120),
+                                             on_activate_call=babase.Call(self._change_camera_target, 'y'))
+        self.target_yminus = bui.buttonwidget(parent=self._root_widget,
+                                              size=button_size,
+                                              label=babase.charstr(babase.SpecialChar.DOWN_ARROW),
+                                              repeat=True,
+                                              button_type='square',
+                                              autoselect=True,
+                                              position=(-482, 2),
+                                              on_activate_call=babase.Call(self._change_camera_target, 'y-'))
+        self._step_text = bui.textwidget(parent=self._root_widget,
                                          scale=0.85,
-                                         color=(1,1,1),
+                                         color=(1, 1, 1),
                                          text='Step:',
                                          size=(0, 0),
                                          position=(450, -38),
                                          h_align='center',
                                          v_align='center')
-            self._text_field = bui.textwidget(
-                                 parent=self._root_widget,
-                                 editable=True,
-                                 size=(100, 40),
-                                 position=(480, -55),
-                                 text='',
-                                 maxwidth=120,
-                                 flatness=1.0,
-                                 autoselect=True,
-                                 v_align='center',
-                                 corner_scale=0.7)
-            self._reset = bui.buttonwidget(parent=self._root_widget,
-                              size=(50,30),
-                              label='Reset',
-                              button_type='square',
-                              autoselect=True,
-                              position=(450, -100),
-                              on_activate_call=babase.Call(self._change_camera_position, 'reset'))
-            self._done = bui.buttonwidget(parent=self._root_widget,
-                              size=(50,30),
-                              label='Done',
-                              button_type='square',
-                              autoselect=True,
-                              position=(520, -100),
-                              on_activate_call=self._close)
-            bui.containerwidget(edit=self._root_widget,
-                           cancel_button=self._done)
+        self._text_field = bui.textwidget(
+            parent=self._root_widget,
+            editable=True,
+            size=(100, 40),
+            position=(480, -55),
+            text='',
+            maxwidth=120,
+            flatness=1.0,
+            autoselect=True,
+            v_align='center',
+            corner_scale=0.7)
+        self._reset = bui.buttonwidget(parent=self._root_widget,
+                                       size=(50, 30),
+                                       label='Reset',
+                                       button_type='square',
+                                       autoselect=True,
+                                       position=(450, -100),
+                                       on_activate_call=babase.Call(self._change_camera_position, 'reset'))
+        self._done = bui.buttonwidget(parent=self._root_widget,
+                                      size=(50, 30),
+                                      label='Done',
+                                      button_type='square',
+                                      autoselect=True,
+                                      position=(520, -100),
+                                      on_activate_call=self._close)
+        bui.containerwidget(edit=self._root_widget,
+                            cancel_button=self._done)
+
     def _close(self):
         bui.containerwidget(edit=self._root_widget,
-            transition=('out_scale'))
+                            transition=('out_scale'))
         MainMenuWindow()
 
     def _change_camera_position(self, direction):
@@ -212,6 +214,7 @@ class Manual_camera_window:
 
 old_refresh_in_game = MainMenuWindow._refresh_in_game
 
+
 def my_refresh_in_game(self, *args, **kwargs):
     value = old_refresh_in_game.__get__(self)(*args, **kwargs)
     camera_button = bui.buttonwidget(
@@ -225,11 +228,14 @@ def my_refresh_in_game(self, *args, **kwargs):
         on_activate_call=self._manual_camera)
     return value
 
+
 def _manual_camera(self):
     bui.containerwidget(edit=self._root_widget, transition='out_scale')
     Manual_camera_window()
 
 # ba_meta export plugin
+
+
 class ByDroopy(babase.Plugin):
     def __init__(self):
         MainMenuWindow._refresh_in_game = my_refresh_in_game
