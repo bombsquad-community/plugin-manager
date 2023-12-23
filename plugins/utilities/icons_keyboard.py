8e8291c5460608aef1d2f8c475714219750851c2
! Freaku <92618708+Freaku17@users.noreply.github.com>
2023-12-23 21:56:50 +0530

    Add files via upload

--- plugins/utilities/icons_keyboard.py
+++ plugins/utilities/icons_keyboard.py
@@ -9,6 +9,7 @@
 
 # ba_meta require api 8
 
+import bauiv1
 import babase
 from babase import charstr
 
@@ -20,15 +21,29 @@ for i in range(26 - (len(list_of_icons) % 26)):
     list_of_icons.append('‎')
 
 
+if hasattr(bauiv1, 'Keyboard'):
+# ba_meta export bauiv1.Keyboard
+    class IconKeyboard(bauiv1.Keyboard):
+        """Keyboard go brrrrrrr"""
+        name = 'Icons by \ue048Freaku'
+        chars = [(list_of_icons[0:10]),
+                 (list_of_icons[10:19]),
+                 (list_of_icons[19:26])]
+        nums = ['‎' for i in range(26)]
+        pages = {
+            f'icon{i//26+1}': tuple(list_of_icons[i:i+26])
+            for i in range(26, len(list_of_icons), 26)
+        }
+else:
 # ba_meta export keyboard
-class IconKeyboard(babase.Keyboard):
-    """Keyboard go brrrrrrr"""
-    name = 'Icons by \ue048Freaku'
-    chars = [(list_of_icons[0:10]),
-             (list_of_icons[10:19]),
-             (list_of_icons[19:26])]
-    nums = ['‎' for i in range(26)]
-    pages = {
-        f'icon{i//26+1}': tuple(list_of_icons[i:i+26])
-        for i in range(26, len(list_of_icons), 26)
-    }
+    class IconKeyboard(babase.Keyboard):
+        """Keyboard go brrrrrrr"""
+        name = 'Icons by \ue048Freaku'
+        chars = [(list_of_icons[0:10]),
+                 (list_of_icons[10:19]),
+                 (list_of_icons[19:26])]
+        nums = ['‎' for i in range(26)]
+        pages = {
+            f'icon{i//26+1}': tuple(list_of_icons[i:i+26])
+            for i in range(26, len(list_of_icons), 26)
+        }
