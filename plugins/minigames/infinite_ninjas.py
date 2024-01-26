546a8d0a4a6ce2863b801c71ebeba6a1b5caedca
brostosjoined <brostosjoined@users.noreply.github.com>
2024-01-26 02:02:31 +0000

    [ci] auto-format

--- plugins/minigames/infinite_ninjas.py
+++ plugins/minigames/infinite_ninjas.py
@@ -1,7 +1,7 @@
 # Ported to api 8 by brostos using baport.(https://github.com/bombsquad-community/baport)
 # ba_meta require api 8
 
-#Copy pasted from ExplodoRun by Blitz,just edited Bots and map 😝
+# Copy pasted from ExplodoRun by Blitz,just edited Bots and map 😝
 
 
 from __future__ import annotations
@@ -19,19 +19,23 @@ if TYPE_CHECKING:
     from typing import Any, Type, Dict, List, Optional
 
 ## MoreMinigames.py support ##
+
+
 def ba_get_api_version():
     return 6
 
+
 def ba_get_levels():
     return [babase._level.Level(
-            'Infinite Ninjas',gametype=InfiniteNinjasGame,
+            'Infinite Ninjas', gametype=InfiniteNinjasGame,
             settings={},
-            preview_texture_name = 'footballStadiumPreview'),
+            preview_texture_name='footballStadiumPreview'),
             babase._level.Level(
-            'Epic Infinite Ninjas',gametype=InfiniteNinjasGame,
-            settings={'Epic Mode':True},
-            preview_texture_name = 'footballStadiumPreview')]
+            'Epic Infinite Ninjas', gametype=InfiniteNinjasGame,
+            settings={'Epic Mode': True},
+            preview_texture_name='footballStadiumPreview')]
 ## MoreMinigames.py support ##
+
 
 class Player(bs.Player['Team']):
     """Our player type for this game."""
@@ -41,6 +45,8 @@ class Team(bs.Team[Player]):
     """Our team type for this game."""
 
 # ba_meta export bascenev1.GameActivity
+
+
 class InfiniteNinjasGame(bs.TeamGameActivity[Player, Team]):
     name = "Infinite Ninjas"
     description = "How long can you survive from Ninjas??"
@@ -49,8 +55,8 @@ class InfiniteNinjasGame(bs.TeamGameActivity[Player, Team]):
                                  scoretype=bs.ScoreType.MILLISECONDS,
                                  lower_is_better=False)
     default_music = bs.MusicType.TO_THE_DEATH
-    
-    def __init__(self, settings:dict):
+
+    def __init__(self, settings: dict):
         settings['map'] = "Football Stadium"
         self._epic_mode = settings.get('Epic Mode', False)
         if self._epic_mode:
@@ -61,27 +67,28 @@ class InfiniteNinjasGame(bs.TeamGameActivity[Player, Team]):
         self._won = False
         self._bots = SpazBotSet()
         self.wave = 1
-    
+
     def on_begin(self) -> None:
         super().on_begin()
-        
+
         self._timer = OnScreenTimer()
         bs.timer(2.5, self._timer.start)
-        
-        #Bots Hehe
-        bs.timer(2.5,self.street)
+
+        # Bots Hehe
+        bs.timer(2.5, self.street)
 
     def street(self):
         for a in range(self.wave):
-            p1 = random.choice([-5,-2.5,0,2.5,5])
-            p3 = random.choice([-4.5,-4.14,-5,-3])
-            time = random.choice([1,1.5,2.5,2])
-            self._bots.spawn_bot(ChargerBot, pos=(p1,0.4,p3),spawn_time = time)
+            p1 = random.choice([-5, -2.5, 0, 2.5, 5])
+            p3 = random.choice([-4.5, -4.14, -5, -3])
+            time = random.choice([1, 1.5, 2.5, 2])
+            self._bots.spawn_bot(ChargerBot, pos=(p1, 0.4, p3), spawn_time=time)
         self.wave += 1
-        
+
     def botrespawn(self):
         if not self._bots.have_living_bots():
             self.street()
+
     def handlemessage(self, msg: Any) -> Any:
 
         # A player has died.
@@ -89,7 +96,7 @@ class InfiniteNinjasGame(bs.TeamGameActivity[Player, Team]):
             super().handlemessage(msg)  # Augment standard behavior.
             self._won = True
             self.end_game()
-        
+
         # A spaz-bot has died.
         elif isinstance(msg, SpazBotDiedMessage):
             # Unfortunately the bot-set will always tell us there are living
@@ -130,5 +137,3 @@ class InfiniteNinjasGame(bs.TeamGameActivity[Player, Team]):
 
         # Ends the activity.
         self.end(results)
-    
-    
--- plugins/minigames/lame_fight.py
+++ plugins/minigames/lame_fight.py
@@ -17,15 +17,18 @@ from bascenev1lib.actor.onscreentimer import OnScreenTimer
 if TYPE_CHECKING:
     from typing import Any, Type, Dict, List, Optional
 
+
 def ba_get_api_version():
     return 6
 
+
 def ba_get_levels():
-	return [babase._level.Level(
-            'Lame Fight',
-			gametype=LameFightGame,
-			settings={},
-			preview_texture_name='courtyardPreview')]
+    return [babase._level.Level(
+        'Lame Fight',
+        gametype=LameFightGame,
+        settings={},
+        preview_texture_name='courtyardPreview')]
+
 
 class Player(bs.Player['Team']):
     """Our player type for this game."""
@@ -35,6 +38,8 @@ class Team(bs.Team[Player]):
     """Our team type for this game."""
 
 # ba_meta export bascenev1.GameActivity
+
+
 class LameFightGame(bs.TeamGameActivity[Player, Team]):
     name = "Lame Fight"
     description = "Save World With Super Powers"
@@ -43,54 +48,61 @@ class LameFightGame(bs.TeamGameActivity[Player, Team]):
                                  scoretype=bs.ScoreType.MILLISECONDS,
                                  lower_is_better=True)
     default_music = bs.MusicType.TO_THE_DEATH
-    
-    def __init__(self, settings:dict):
+
+    def __init__(self, settings: dict):
         settings['map'] = "Courtyard"
         super().__init__(settings)
         self._timer: Optional[OnScreenTimer] = None
         self._winsound = bs.getsound('score')
         self._won = False
         self._bots = SpazBotSet()
-    
+
     def on_begin(self) -> None:
         super().on_begin()
-        
+
         self._timer = OnScreenTimer()
         bs.timer(4.0, self._timer.start)
-        
-        #Bots Hehe
-        bs.timer(1.0, lambda: self._bots.spawn_bot(ChargerBot, pos=(3,3,-2),spawn_time = 3.0))
-        bs.timer(1.0, lambda: self._bots.spawn_bot(ChargerBot, pos=(-3,3,-2),spawn_time = 3.0))
-        bs.timer(1.0, lambda: self._bots.spawn_bot(ChargerBot, pos=(5,3,-2),spawn_time = 3.0))
-        bs.timer(1.0, lambda: self._bots.spawn_bot(ChargerBot, pos=(-5,3,-2),spawn_time = 3.0))
-        bs.timer(1.0, lambda: self._bots.spawn_bot(ChargerBot, pos=(0,3,1),spawn_time = 3.0))
-        bs.timer(1.0, lambda: self._bots.spawn_bot(ChargerBot, pos=(0,3,-5),spawn_time = 3.0))
-        bs.timer(9.0, lambda: self._bots.spawn_bot(BomberBotProShielded, pos=(-7,5,-7.5),spawn_time = 3.0))
-        bs.timer(9.0, lambda: self._bots.spawn_bot(BomberBotProShielded, pos=(7,5,-7.5),spawn_time = 3.0))
-        bs.timer(9.0, lambda: self._bots.spawn_bot(BomberBotProShielded, pos=(7,5,1.5),spawn_time = 3.0))
-        bs.timer(9.0, lambda: self._bots.spawn_bot(BomberBotProShielded, pos=(-7,5,1.5),spawn_time = 3.0))
-        bs.timer(12.0, lambda: self._bots.spawn_bot(TriggerBotProShielded, pos=(-1,7,-8),spawn_time = 3.0))
-        bs.timer(12.0, lambda: self._bots.spawn_bot(TriggerBotProShielded, pos=(1,7,-8),spawn_time = 3.0))
-        bs.timer(15.0, lambda: self._bots.spawn_bot(ExplodeyBot, pos=(0,3,-5),spawn_time = 3.0))
-        bs.timer(20.0, lambda: self._bots.spawn_bot(ExplodeyBot, pos=(0,3,1),spawn_time = 3.0))
-        bs.timer(20.0, lambda: self._bots.spawn_bot(ExplodeyBot, pos=(-5,3,-2),spawn_time = 3.0))
-        bs.timer(20.0, lambda: self._bots.spawn_bot(ExplodeyBot, pos=(5,3,-2),spawn_time = 3.0))
-        bs.timer(30,self.street)
+
+        # Bots Hehe
+        bs.timer(1.0, lambda: self._bots.spawn_bot(ChargerBot, pos=(3, 3, -2), spawn_time=3.0))
+        bs.timer(1.0, lambda: self._bots.spawn_bot(ChargerBot, pos=(-3, 3, -2), spawn_time=3.0))
+        bs.timer(1.0, lambda: self._bots.spawn_bot(ChargerBot, pos=(5, 3, -2), spawn_time=3.0))
+        bs.timer(1.0, lambda: self._bots.spawn_bot(ChargerBot, pos=(-5, 3, -2), spawn_time=3.0))
+        bs.timer(1.0, lambda: self._bots.spawn_bot(ChargerBot, pos=(0, 3, 1), spawn_time=3.0))
+        bs.timer(1.0, lambda: self._bots.spawn_bot(ChargerBot, pos=(0, 3, -5), spawn_time=3.0))
+        bs.timer(9.0, lambda: self._bots.spawn_bot(
+            BomberBotProShielded, pos=(-7, 5, -7.5), spawn_time=3.0))
+        bs.timer(9.0, lambda: self._bots.spawn_bot(
+            BomberBotProShielded, pos=(7, 5, -7.5), spawn_time=3.0))
+        bs.timer(9.0, lambda: self._bots.spawn_bot(
+            BomberBotProShielded, pos=(7, 5, 1.5), spawn_time=3.0))
+        bs.timer(9.0, lambda: self._bots.spawn_bot(
+            BomberBotProShielded, pos=(-7, 5, 1.5), spawn_time=3.0))
+        bs.timer(12.0, lambda: self._bots.spawn_bot(
+            TriggerBotProShielded, pos=(-1, 7, -8), spawn_time=3.0))
+        bs.timer(12.0, lambda: self._bots.spawn_bot(
+            TriggerBotProShielded, pos=(1, 7, -8), spawn_time=3.0))
+        bs.timer(15.0, lambda: self._bots.spawn_bot(ExplodeyBot, pos=(0, 3, -5), spawn_time=3.0))
+        bs.timer(20.0, lambda: self._bots.spawn_bot(ExplodeyBot, pos=(0, 3, 1), spawn_time=3.0))
+        bs.timer(20.0, lambda: self._bots.spawn_bot(ExplodeyBot, pos=(-5, 3, -2), spawn_time=3.0))
+        bs.timer(20.0, lambda: self._bots.spawn_bot(ExplodeyBot, pos=(5, 3, -2), spawn_time=3.0))
+        bs.timer(30, self.street)
 
     def street(self):
-        bs.broadcastmessage("Lame Guys Are Here!",color = (1,0,0))
-        for a in range(-1,2):
-            for b in range(-3,0):
-                self._bots.spawn_bot(BrawlerBotProShielded, pos=(a,3,b),spawn_time = 3.0)
-        
+        bs.broadcastmessage("Lame Guys Are Here!", color=(1, 0, 0))
+        for a in range(-1, 2):
+            for b in range(-3, 0):
+                self._bots.spawn_bot(BrawlerBotProShielded, pos=(a, 3, b), spawn_time=3.0)
+
     def spawn_player(self, player: Player) -> bs.Actor:
         spawn_center = (0, 3, -2)
         pos = (spawn_center[0] + random.uniform(-1.5, 1.5), spawn_center[1],
                spawn_center[2] + random.uniform(-1.5, 1.5))
-        spaz = self.spawn_player_spaz(player,position = pos)
-        p = ["Bigger Blast","Stronger Punch","Shield","Speed"]
+        spaz = self.spawn_player_spaz(player, position=pos)
+        p = ["Bigger Blast", "Stronger Punch", "Shield", "Speed"]
         Power = random.choice(p)
-        spaz.bomb_type = random.choice(["normal","sticky","ice","impact","normal","ice","sticky"])
+        spaz.bomb_type = random.choice(
+            ["normal", "sticky", "ice", "impact", "normal", "ice", "sticky"])
         bs.broadcastmessage(f"Now You Have {Power}")
         if Power == p[0]:
             spaz.bomb_count = 3
@@ -103,10 +115,12 @@ class LameFightGame(bs.TeamGameActivity[Player, Team]):
         if Power == p[3]:
             spaz.node.hockey = True
         return spaz
+
     def _check_if_won(self) -> None:
         if not self._bots.have_living_bots():
             self._won = True
             self.end_game()
+
     def handlemessage(self, msg: Any) -> Any:
 
         # A player has died.
@@ -154,5 +168,3 @@ class LameFightGame(bs.TeamGameActivity[Player, Team]):
 
         # Ends the activity.
         self.end(results)
-    
-    
