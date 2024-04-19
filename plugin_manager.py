88834bd9795538b4a8f963a873affcbd359f5ba3
Vishal <vishal.u338@gmail.com>
2024-04-20 02:22:22 +0530

    Update - 1.0.9

--- plugin_manager.py
+++ plugin_manager.py
@@ -32,7 +32,7 @@ from threading import Thread
 import logging
 
 
-PLUGIN_MANAGER_VERSION = "1.0.8"
+PLUGIN_MANAGER_VERSION = "1.0.9"
 REPOSITORY_URL = "https://github.com/bombsquad-community/plugin-manager"
 # Current tag can be changed to "staging" or any other branch in
 # plugin manager repo for testing purpose.
@@ -1897,7 +1897,7 @@ class PluginManagerWindow(bui.Window):
                                          always_highlight=True,
                                          color=color,
                                          # on_select_call=lambda: None,
-                                         text=plugin.name,
+                                         text=plugin.name.replace('_', ' ').title(),
                                          click_activate=True,
                                          on_activate_call=lambda: self.show_plugin_window(plugin),
                                          h_align='left',
