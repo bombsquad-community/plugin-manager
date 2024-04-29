e7646c9dc4b42f90e91a062ab46d71350a8e8b67
Vishal <vishal.u338@gmail.com>
2024-04-29 20:29:30 +0530

    Update plugin_manager.py

--- plugin_manager.py
+++ plugin_manager.py
@@ -2080,7 +2080,11 @@ class PluginManagerWindow(bui.Window):
             return
 
         try:
-            category_plugins = await self.plugin_manager.categories[category if category != 'Installed' else 'All'].get_plugins()
+             if self.plugin_manager.categories != {}:
+                if self.plugin_manager.categories['All'] is not None:
+                    category_plugins = await self.plugin_manager.categories[category if category != 'Installed' else 'All'].get_plugins()
+                else: return
+            else: return
         except (KeyError, AttributeError):
             no_internet_text = "Make sure you are connected\n to the Internet and try again."
             if bui.textwidget(query=self._plugin_manager_status_text) != no_internet_text:
