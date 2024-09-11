d51c10c8ce4b8f6ecae26853be5fda13d5745712
Y-ellowDev <Y-ellowDev@users.noreply.github.com>
2024-09-11 21:46:37 +0000

    [ci] auto-format

--- plugins/utilities/achievements_unlocker.py
+++ plugins/utilities/achievements_unlocker.py
@@ -6,28 +6,30 @@ import bascenev1 as bs
 
 ACHIEVEMENTS = bui.app.classic.ach.achievements
 
+
 def unlock_all_achievements():
     if bui.app.plus.get_v1_account_state() != 'signed_in':
         return bui.screenmessage(bui.Lstr(resource='notSignedInErrorText'), color=(1, 0, 0))
-    
+
     UNLOCKED = False
     for num, achievement in enumerate(ACHIEVEMENTS, start=1):
         if not achievement.complete:
-            bui.app.plus.add_v1_account_transaction({'type': 'ACHIEVEMENT', 'name': achievement.name})
+            bui.app.plus.add_v1_account_transaction(
+                {'type': 'ACHIEVEMENT', 'name': achievement.name})
             achievement.set_complete()
             bui.screenmessage(f"- Unlocked ({achievement.name})", color=(1, 1, 0))
             UNLOCKED = True
-            
-        
+
     if UNLOCKED:
         bui.screenmessage("Unlocked All Achievements !", color=(0, 1, 0))
-            
+
 
 def _have_pro():
     return True
 
 # ba_meta require api 8
 # ba_meta export plugin
+
 
 class ByYelllow(babase.Plugin):
     def on_app_running(self) -> None:
