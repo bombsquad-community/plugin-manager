c71dac4b2d23b5cf5c3801cbc528a44c2e1e8046
brostosjoined <67740566+brostosjoined@users.noreply.github.com>
2025-07-19 21:48:43 +0000

    [ci] auto-format

--- plugins/maps/shake_em_off.py
+++ plugins/maps/shake_em_off.py
@@ -1,28 +1,24 @@
 # ba_meta require api 9
+import babase
 ba_plugman = {
     "shake_em_off": {
-      "description": "Shake as many stickies of you as possible",
-      "external_url": "",
-      "authors": [
-        {
-          "name": "brostosjoined",
-          "email": "",
-          "discord": "brostos"
+        "description": "Shake as many stickies of you as possible",
+        "external_url": "",
+        "authors": [
+            {
+                "name": "brostosjoined",
+                "email": "",
+                "discord": "brostos"
+            }
+        ],
+        "versions": {
+            "1.1.0": None
         }
-      ],
-      "versions": {
-        "1.1.0": None
-      }
     }
 }
-
-import babase
-
-
 
 
 # ba_meta export babase.Plugin
 class bybrostos(babase.Plugin):
     def __init__():
         pass
-
