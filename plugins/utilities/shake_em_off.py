fa7ad5701ef2f967b4c937cc454b6d7b6cafda86
brostosjoined <rovermigente44@gmail.com>
2025-07-20 00:47:57 +0300

    Autometadata Insertion

---
+++ plugins/utilities/shake_em_off.py
@@ -0,0 +1,28 @@
+# ba_meta require api 9
+ba_plugman = {
+    "shake_em_off": {
+      "description": "Shake as many stickies of you as possible",
+      "external_url": "",
+      "authors": [
+        {
+          "name": "brostosjoined",
+          "email": "",
+          "discord": "brostos"
+        }
+      ],
+      "versions": {
+        "1.1.0": None
+      }
+    }
+}
+
+import babase
+
+
+
+
+# ba_meta export babase.Plugin
+class bybrostos(babase.Plugin):
+    def __init__():
+        pass
+
