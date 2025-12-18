8fe514c2b77c68557d4ad7c9ccb0dd059a2020d5
Loup-Garou911XD <90267658+Loup-Garou911XD@users.noreply.github.com>
2025-12-18 12:29:43 +0530

    Updated store_event_special plugin

--- plugins/utilities/store_event_specials.py
+++ plugins/utilities/store_event_specials.py
@@ -27,7 +27,8 @@ from typing import List, Dict, Any
 
 import babase
 import bauiv1 as bui
-
+from bauiv1lib.store.browser import StoreBrowserWindow
+from bauiv1._appsubsystem import UIV1AppSubsystem
 original_get_store_layout = bui.app.classic.store.get_store_layout
 
 
@@ -64,3 +65,22 @@ def modified_get_store_layout() -> Dict[str, List[Dict[str, Any]]]:
 class Main(babase.Plugin):
     def on_app_running(self) -> None:
         bui.app.classic.store.get_store_layout = modified_get_store_layout
+    def has_settings_ui(self):
+        return True
+    def show_settings_ui(self, button):
+        try:
+            main_window = UIV1AppSubsystem().get_main_window()
+            if main_window:
+                main_window.main_window_replace(lambda:StoreBrowserWindow(
+                    show_tab=StoreBrowserWindow.TabID.MINIGAMES,
+                    origin_widget=button,
+                    )
+                )
+            else:
+                StoreBrowserWindow(
+                    show_tab=StoreBrowserWindow.TabID.MINIGAMES,
+                    origin_widget=button,
+                )
+        except Exception as e:
+            print(e)
+
