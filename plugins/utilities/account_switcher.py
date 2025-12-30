239a237e8ffd0ad5ae1dbd7cc5f21e7b4470422d
Loup-Garou911XD <90267658+Loup-Garou911XD@users.noreply.github.com>
2025-12-30 13:37:18 +0530

    account_switcher updated to save config on switch

--- plugins/utilities/account_switcher.py
+++ plugins/utilities/account_switcher.py
@@ -229,6 +229,7 @@ class AccountSwitcherUI(bui.Window):
             return
         account_folder = path.join(ACCOUNTS_DIR, self._selected_profile)
 
+        self.save_current_account()
         def do_switch():
             for fname in ACCOUNT_FILES:
                 dest = path.join(USER_DIR, fname)
