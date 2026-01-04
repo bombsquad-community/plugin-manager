819a072b494cd2498226e2c8da9bc82447e15bd9
Loup-Garou911XD <90267658+Loup-Garou911XD@users.noreply.github.com>
2026-01-05 03:00:15 +0530

    autometa commit

--- test/versioning_tools.py
+++ test/versioning_tools.py
@@ -1,22 +1,25 @@
 import sys
-from get_latest import get_latest_version
+from get_latest import get_latest_plugman_version
+
+"""if called directly from command line, it will check if given version is lower
+than latest version in index.json"""
 
 
 def semantic_to_str(semantic_version: str):
     """Convert version in the form of v1.2.3 to 001002003
     for comparing 2 version in semantic versioning format"""
     out = ""
-    for i in (semantic_version.split(".")):
+    for i in semantic_version.split("."):
         if len(i) == 1:
-            out += "00"+i
+            out += "00" + i
         if len(i) == 2:
-            out += "0"+i
+            out += "0" + i
     return out
 
 
-def version_is_lower(version: str):
+def plugman_version_is_lower_than(version: str):
     """Check if given version is lower than the latest entry in index.json"""
-    latest = semantic_to_str(get_latest_version())
+    latest = semantic_to_str(get_latest_plugman_version())
     version = semantic_to_str(version)
     if latest > version:
         return True
@@ -30,5 +33,5 @@ if __name__ == "__main__":
         sys.exit(1)
 
     version = sys.argv[1].replace("v", "", 1)
-    out = version_is_lower(version)
+    out = plugman_version_is_lower_than(version)
     print(int(out))
