eff06c942ead93b143b3f9a6034392b4d074c454
Loup-Garou911XD <90267658+Loup-Garou911XD@users.noreply.github.com>
2026-01-05 03:00:15 +0530

    autometa commit

--- test/get_latest.py
+++ test/get_latest.py
@@ -2,7 +2,7 @@ import json
 import sys
 
 
-def get_latest_version():
+def get_latest_plugman_version() -> str:
     """Get latest version entry from index.json"""
     with open('index.json', 'r') as file:
         content = json.loads(file.read())
@@ -10,16 +10,28 @@ def get_latest_version():
         return latest_version
 
 
+def get_latest_plugin_version(plugin_name, json_path) -> str:
+    """Get latest version entry from json file for a specific plugin"""
+    with open(json_path, 'r') as file:
+        content = json.loads(file.read())
+        latest_version = list(content["plugins"][plugin_name]["versions"].keys())[0]
+        return latest_version
+
+
 def get_latest_api():
     """Get latest api entry from index.json"""
     with open('index.json', 'r') as file:
         content = json.loads(file.read())
-        latest_api = content["versions"][get_latest_version()]["api_version"]
+        latest_api = content["versions"][get_latest_plugman_version()]["api_version"]
         return latest_api
 
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print(f"Usage: python3 {__file__.split('/')[-1]} function")
+        print(f"Usage: python3 {__file__.split('/')[-1]} function [args...]")
         sys.exit(1)
-    print(globals()[sys.argv[1]]())  # used to call the fucntion passed as cli parameter
+    function_name = sys.argv[1]
+    function_args = sys.argv[2:]
+    print(
+        globals()[function_name](*function_args)
+    )  # used to call the function passed as cli parameter with additional arguments
